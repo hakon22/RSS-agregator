21389eab0388edf2fb25d8e135849c06c3e5b339
hakon2 <hakon1@mail.ru>
2023-03-03 18:50:42 +0300

    project css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
-import './style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import './style.css';
 // import Icon from './avocado.png';
 
 const component = () => {
